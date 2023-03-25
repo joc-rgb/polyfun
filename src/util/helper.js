5afb8c162069d5c475d5ebc4ba1baa419fe56014
jocxy <jocelinxy@gmail.com>
2023-03-25 20:58:55 +0800

    Random Id generator

---
+++ src/util/helper.js
@@ -0,0 +1,12 @@
+//source: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+export function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
