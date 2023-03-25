206d27387bc07f2fdb929a7509657d235ce37d91
jocxy <jocelinxy@gmail.com>
2023-03-25 20:58:41 +0800

    MeetUp component

--- src/components/MeetUpItem.tsx
+++ src/components/MeetUpItem.tsx
@@ -5,8 +5,8 @@ const MeetUpItem = (post) => {
   return (
     <div className='bg-slate-100 rounded-xl p-3'>
         <p className='font-semibold text-lg'>{post.post.name}</p>
-        <div>
-        <p className='font-semibold text-sm p-2 bg-slate-300 rounded-2xl ' >{post.post.location}</p></div>
+        <div className='max-w-fit'>
+        <p className='font-semibold text-sm p-2 py-1 bg-slate-300 rounded-2xl ' >{post.post.location}</p></div>
         <p>{post.post.desc}</p>
     </div>
   )
